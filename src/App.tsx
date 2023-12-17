01f609ec596738287e3d097d03ab9029caa5bf9a
Matias Marinelli <matiasr86@hotmail.com>
2023-12-17 15:24:59 -0300

    Cambio en el ruteo archivo App, se agrego el componente Oulet en el Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,54 @@
-import {Routes, Route} from "react-router-dom"
-import RequireAuth from "./components/RequireAuth"
-import Layout from "./components/Layout"
+import { Routes, Route } from "react-router-dom";
+import RequireAuth from "./components/RequireAuth";
+import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import About from "./pages/About"
+import About from "./pages/About";
 import Books from "./pages/Books";
 import AddBook from "./pages/AddBook";
-import EditBook from "./pages/EditBook"
+import EditBook from "./pages/EditBook";
 import Footer from "./components/Footer";
 import NotFound from "./pages/NotFound";
 function App() {
-
   return (
     <>
-      <Layout/>
-      <h4 className="text-center p-3 fs-1 m-0 bg-dark text-light ">Librería Online Mcga</h4>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
-        <Route path="/about" element={<About/>}></Route>
-        <Route path="/books" element={<RequireAuth><Books/></RequireAuth>}></Route>
-        <Route path="/books/add" element={<RequireAuth><AddBook/></RequireAuth>}></Route>
-        <Route path="/books/edit/:id" element={<RequireAuth><EditBook/></RequireAuth>}></Route>
-        <Route path="*" element={<NotFound/>}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route
+            path="/books"
+            element={
+              <RequireAuth>
+                <Books />
+              </RequireAuth>
+            }
+          ></Route>
+          <Route
+            path="/books/add"
+            element={
+              <RequireAuth>
+                <AddBook />
+              </RequireAuth>
+            }
+          ></Route>
+          <Route
+            path="/books/edit/:id"
+            element={
+              <RequireAuth>
+                <EditBook />
+              </RequireAuth>
+            }
+          ></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <h4 className="text-center p-3 fs-1 m-0 bg-dark text-light ">
+        Librería Online Mcga
+      </h4>
+      <Outlet/>
     </>
   );
 };
