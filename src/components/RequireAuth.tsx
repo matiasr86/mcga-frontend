586199ebedee43fb9c78a9165d3fd225665f6439
Alejandro Depetris <aledepetris@outlook.com>
2023-12-20 21:05:26 -0300

    fix en autenticación

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
-import { signOut } from "firebase/auth";
+import { getAuth, signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { useAuth } from "../context/AuthProvider";
 
@@ -8,18 +8,25 @@ import { useAuth } from "../context/AuthProvider";
 const RequireAuth: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { isLoggedIn } = useAuth();
   const { setIsLoggedIn } = useAuth();
-
-  if (!isLoggedIn || !localStorage.getItem('Token'))  {
+  const tokenGuardado = localStorage.getItem('Token');
+  if (!isLoggedIn || !tokenGuardado)  {
     setIsLoggedIn(false)
     signOut(auth);
     localStorage.removeItem("Token");
     localStorage.removeItem("User");
-
-
-    return <Navigate to={"/login"} />;
-    
+    return <Navigate to={"/login"} />;  
   }
-
+  else {
+    const authorization = getAuth();
+    authorization.currentUser?.getIdToken().then((res : any)=>{
+      if(res !== localStorage.getItem('Token')){
+        setIsLoggedIn(false)
+        signOut(auth);
+        localStorage.removeItem("Token");
+        localStorage.removeItem("User");
+        return <Navigate to={"/login"} />;  
+    }})
+}
   return children;
 };
 
